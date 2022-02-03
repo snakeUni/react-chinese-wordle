7159db0cfabec573128eae2a7058ed293e29987c
Uni <1025687605@qq.com>
2022-02-03 20:48:01 +0800

    add

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -4,40 +4,8 @@ import {
   MoonIcon,
   SunIcon
 } from '@chakra-ui/icons'
-import { useEffect, useState } from 'react'
 
 export default function Header() {
-  const [theme, setTheme] = useState(() => {
-    if (typeof document === 'undefined') {
-      return 'light'
-    }
-
-    return document.documentElement.classList.contains('dark')
-      ? 'dark'
-      : 'light'
-  })
-  useEffect(() => {
-    const theme = localStorage.getItem('theme')
-    if (theme === 'dark') {
-      setTheme('dark')
-      document.documentElement.classList.add('dark')
-    }
-  }, [])
-
-  const toggleTheme = () => {
-    const theme = localStorage.getItem('theme')
-
-    if (theme === 'dark') {
-      document.documentElement.classList.remove('dark')
-      localStorage.setItem('theme', 'light')
-      setTheme('light')
-    } else {
-      document.documentElement.classList.add('dark')
-      localStorage.setItem('theme', 'dark')
-      setTheme('dark')
-    }
-  }
-
   return (
     <div className="p-4 flex items-center border-solid border-b border-gray-200 font-serif text-2xl font-medium dark:text-slate-100">
       <div className={'w-16 flex items-center justify-start'}>
@@ -48,19 +16,22 @@ export default function Header() {
       </div>
       <div className={'w-16 flex gap-2 justify-end'}>
         <SettingsIcon className="cursor-pointer" fontSize={20} />
-        {theme === 'light' ? (
-          <MoonIcon
-            className="cursor-pointer"
-            fontSize={20}
-            onClick={toggleTheme}
-          />
-        ) : (
-          <SunIcon
-            className="cursor-pointer"
-            fontSize={20}
-            onClick={toggleTheme}
-          />
-        )}
+        <div
+          className="cursor-pointer flex dark:hidden"
+          onClick={() => {
+            ;(window as any).__setPreferredTheme('dark')
+          }}
+        >
+          <MoonIcon fontSize={20} />
+        </div>
+        <div
+          className="cursor-pointer hidden dark:flex"
+          onClick={() => {
+            ;(window as any).__setPreferredTheme('light')
+          }}
+        >
+          <SunIcon fontSize={20} />
+        </div>
       </div>
     </div>
   )
